8a19a4fda082d6075ca5db07635abed1e17c7182
Schlumpf, Stefanie <stefanie.schlumpf2@zuehlke.com>
2021-09-20 15:34:54 +0200

    add host button and list of games to chooseGame screen

--- frontend/src/components/ChooseGame/ChooseGame.tsx
+++ frontend/src/components/ChooseGame/ChooseGame.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import './ChooseGame.css';
 
+const games = [
+    "Game Steffi",
+    "Game Dani",
+    "Game Nicolas"
+];
+
 const ChooseGame = () => (
-  <div className="ChooseGame">
-    ChooseGame Component
-  </div>
+    <div className="ChooseGame">
+        <button id="btnHostGame" onClick={() => console.log("Host Game")}>Host New Game</button>
+        <hr/>
+        <div className="flex-container">
+            <table>
+                <tbody>
+                {games.map(game => {
+                    return <tr key={game}>
+                        <td>{game}</td>
+                    </tr>
+                })}
+                </tbody>
+            </table>
+            <button id="btnJoinGame" onClick={() => console.log("Join Game")}>Join Game</button>
+        </div>
+    </div>
 );
 
 export default ChooseGame;
