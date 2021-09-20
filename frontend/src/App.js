fc006858a9140d44ec27ef603567f77fdbbd8cbc
Nic <nicolas.odermatt@zuehlke.com>
2021-09-20 14:58:32 +0200

    Wrap components in div

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import ChooseName from "./components/ChooseName/ChooseName";
+import Lobby from "./components/Lobby/Lobby";
+import ChooseGame from "./components/ChooseGame/ChooseGame";
+import CreateTasks from "./components/CreateTasks/CreateTasks";
 
 function App() {
   return (
+      <div>
     <ChooseName/>
+    <ChooseGame/>
+    <Lobby/>
+    <CreateTasks/>
+      </div>
   );
 }
 
