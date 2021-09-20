4d957422ebfde670e6c33f4addf238a3d5a62214
Cagatay Özyurt <cagatayoezyurt@gmail.com>
2021-09-20 14:08:29 +0200

    Games Backend Project Initialization Spring Boot Kotlin

---
+++ games/src/test/kotlin/com/zuehlke/amongeus/games/PlayersApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.zuehlke.amongeus.games
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlayersApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
