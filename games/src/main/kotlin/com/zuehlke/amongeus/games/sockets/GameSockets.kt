992098ac3735392e683f6851cee7d7489505b4e2
genm <test>
2021-09-20 16:12:05 +0200

    Initial working STOMP socket config.

--- games/src/main/kotlin/com/zuehlke/amongeus/games/sockets/GameSockets.kt
+++ games/src/main/kotlin/com/zuehlke/amongeus/games/sockets/GameSockets.kt
@@ -3,15 +3,16 @@ package com.zuehlke.amongeus.games.sockets
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.stereotype.Controller
-import org.springframework.web.util.HtmlUtils
 
 
 @Controller
 class GameSockets {
 
     @MessageMapping("/game")
+    @SendTo("/topic/result")
     @Throws(Exception::class)
-    fun greeting(message: String) {
+    fun greeting(message: String): String {
         println(message);
+        return "success";
     }
 }
