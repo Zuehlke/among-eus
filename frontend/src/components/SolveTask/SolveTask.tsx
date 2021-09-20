83c33a50d8e6dc92ba684934ebf31cc8f0c8066e
Nic <nicolas.odermatt@zuehlke.com>
2021-09-20 14:53:52 +0200

    Add component skeleton

---
+++ frontend/src/components/SolveTask/SolveTask.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './SolveTask.css';
+
+const SolveTask = () => (
+  <div className="SolveTask">
+    SolveTask Component
+  </div>
+);
+
+export default SolveTask;
---
+++ frontend/src/components/TasksMap/TasksMap.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './TasksMap.css';
+
+const TasksMap = () => (
+  <div className="TasksMap">
+    TasksMap Component
+  </div>
+);
+
+export default TasksMap;
