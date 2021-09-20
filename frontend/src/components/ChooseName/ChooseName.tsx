0874f3ce0c66288c1adf569db964c51dcb669c53
Nic <nicolas.odermatt@zuehlke.com>
2021-09-20 14:39:28 +0200

    Create first component

---
+++ frontend/src/components/ChooseName/ChooseName.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ChooseName.css';
+
+const ChooseName = () => (
+  <div className="ChooseName">
+    ChooseName Component
+  </div>
+);
+
+export default ChooseName;
