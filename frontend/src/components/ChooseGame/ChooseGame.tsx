92a789944c894fc46854b223483444f086abad8f
Schlumpf, Stefanie <stefanie.schlumpf2@zuehlke.com>
2021-09-21 08:41:51 +0200

    send game id on click on btnJoinGame

--- frontend/src/components/ChooseGame/ChooseGame.tsx
+++ frontend/src/components/ChooseGame/ChooseGame.tsx
@@ -1,29 +1,39 @@
 import React from 'react';
 import './ChooseGame.css';
 
-const games = [
-    "Game Steffi",
-    "Game Dani",
-    "Game Nicolas"
+interface Game {
+    id: number;
+    name: string;
+}
+
+const games: Game[] = [
+    {id: 1, name: "Game Steffi"},
+    {id: 2, name: "Game Dani"},
+    {id: 3, name: "Game Nicolas"}
 ];
 
+let selectedGameId: number;
+function selectGameId(id: number){
+    selectedGameId = id;
+}
+
 const ChooseGame = () => (
-    <div className="ChooseGame">
-        <button id="btnHostGame" onClick={() => console.log("Host Game")}>Host New Game</button>
-        <hr/>
-        <div className="flex-container">
-            <table>
-                <tbody>
-                {games.map(game => {
-                    return <tr key={game}>
-                        <td>{game}</td>
-                    </tr>
-                })}
-                </tbody>
-            </table>
-            <button id="btnJoinGame" onClick={() => console.log("Join Game")}>Join Game</button>
+        <div className="ChooseGame">
+            <button id="btnHostGame" onClick={() => console.log("Host Game")}>Host New Game</button>
+            <hr/>
+            <div className="flex-container">
+                <table>
+                    <tbody>
+                    {games.map(game => {
+                        return <tr key={game.id} onClick={() => selectGameId(game.id)}>
+                            <td>{game.name}</td>
+                        </tr>
+                    })}
+                    </tbody>
+                </table>
+                <button id="btnJoinGame" onClick={() => console.log(selectedGameId)}>Join Game</button>
+            </div>
         </div>
-    </div>
-);
+    );
 
 export default ChooseGame;
