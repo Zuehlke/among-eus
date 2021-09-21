bce0c5275db52c313f6a5ef5ef7462b6761bfbd6
Andrea Mathis <andreamathis@users.noreply.github.com>
2021-09-21 15:39:35 +0200

    bootstrap-servers also for admin-client

--- players/src/main/kotlin/com/zuehlke/amongeus/players/kafka/KafkaTopicConfiguration.kt
+++ players/src/main/kotlin/com/zuehlke/amongeus/players/kafka/KafkaTopicConfiguration.kt
@@ -7,15 +7,13 @@ import org.springframework.kafka.config.TopicBuilder
 
 @Configuration
 class KafkaTopicConfiguration {
-
     @Bean
     fun topicExample(): NewTopic {
         println("Creating topics ...")
         return TopicBuilder.name("mytopic")
-                .partitions(2)
-                .replicas(2)
-                .build()
+            .partitions(1)
+            .replicas(1)
+            .build()
         println("Creating topics ... DONE")
     }
-
 }
