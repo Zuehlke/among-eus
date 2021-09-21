0da504142a576d26f22a23772aca9e50cb8f1d56
Daniel Suter <suter_daniel@hotmail.com>
2021-09-21 15:34:02 +0200

    Merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,9 @@ import Lobby from "./components/Lobby/Lobby";
 import ChooseGame from "./components/ChooseGame/ChooseGame";
 import CreateTasks from "./components/CreateTasks/CreateTasks";
 import React from "react";
+import SocketConnection from "./components/SocketConnection/SocketConnection";
 
 class App extends React.Component {
-
 
     constructor(props, context) {
         super(props, context);
@@ -23,6 +23,7 @@ class App extends React.Component {
                 {currentView === 'ChooseGame' && <ChooseGame/>}
                 {currentView === 'Lobby' && <Lobby/>}
                 {currentView === 'CreateTasks' && <CreateTasks/>}
+                {currentView === 'SocketConnection' && <SocketConnection/>}
             </div>
         );
     }
