4fb36994c0937fe35a580efd649b9c8f96c72fd4
Daniel Suter <suter_daniel@hotmail.com>
2021-09-21 09:07:19 +0200

    Trivial version of create tasks without GPS.

--- frontend/src/components/CreateTasks/CreateTasks.tsx
+++ frontend/src/components/CreateTasks/CreateTasks.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 import './CreateTasks.css';
+import emptyImage from './empty.jpeg';
 
 const CreateTasks = () => (
-  <div className="CreateTasks">
-    CreateTasks Component
-  </div>
+    <div className="CreateTasks">
+        <div>Go to a location and take a picture for others to find.</div>
+        {[1, 2].map(taskIndex => {
+            return <div key={taskIndex}>
+                <div>Task {taskIndex}</div>
+                <div>
+                    <div>
+                        <input type="file" accept="image/*"
+                               onChange={(event) => {
+                                   onPhotoUpload(event, taskIndex)
+                               }}/>
+                    </div>
+                    <div>
+                        <img id={'taskImage' + taskIndex} src={emptyImage} alt={'Task ' + taskIndex} width="200"/>
+                    </div>
+                </div>
+            </div>
+        })}:
+
+    </div>
 );
+
+function onPhotoUpload(event: ChangeEvent<HTMLInputElement>, taskIndex: number) {
+    // @ts-ignore
+    const file = event.target.files[0];
+    // @ts-ignore
+    document.getElementById('taskImage' + taskIndex).src = URL.createObjectURL(file);
+}
 
 export default CreateTasks;
