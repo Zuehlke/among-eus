4cdae0e234331948eed6386a1eb2496bfa8e5825
Daniel Suter <suter_daniel@hotmail.com>
2021-09-21 13:33:53 +0200

    Conditional display of views.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,29 @@ import ChooseName from "./components/ChooseName/ChooseName";
 import Lobby from "./components/Lobby/Lobby";
 import ChooseGame from "./components/ChooseGame/ChooseGame";
 import CreateTasks from "./components/CreateTasks/CreateTasks";
+import React from "react";
 
-function App() {
-  return (
-      <div>
-    <ChooseName/>
-    <ChooseGame/>
-    <Lobby/>
-    <CreateTasks/>
-      </div>
-  );
+class App extends React.Component {
+
+
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            currentView: 'ChooseName',
+        };
+    }
+
+    render() {
+        const currentView = this.state.currentView;
+        return (
+            <div>
+                {currentView === 'ChooseName' && <ChooseName/>}
+                {currentView === 'ChooseGame' && <ChooseGame/>}
+                {currentView === 'Lobby' && <Lobby/>}
+                {currentView === 'CreateTasks' && <CreateTasks/>}
+            </div>
+        );
+    }
 }
 
 export default App;
