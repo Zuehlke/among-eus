6f5349ede1752fb8eaae9b5c504f7d0ac7e27f4f
Rolf Bruderer <bruderer@computerscience.ch>
2021-09-21 17:18:01 +0200

    remove unused annotation

--- players/src/main/kotlin/com/zuehlke/amongeus/players/kafka/KafkaController.kt
+++ players/src/main/kotlin/com/zuehlke/amongeus/players/kafka/KafkaController.kt
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.Future
-import javax.annotation.PostConstruct
 
 @RestController
 class KafkaController {
