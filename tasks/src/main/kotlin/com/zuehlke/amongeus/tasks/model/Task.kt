b83581ed1dce69ba837b361adc53359161b1c569
genm <test>
2021-09-21 13:25:32 +0200

    WIP: Introduce Kafka publisher for Tasks.

--- tasks/src/main/kotlin/com/zuehlke/amongeus/tasks/model/Task.kt
+++ tasks/src/main/kotlin/com/zuehlke/amongeus/tasks/model/Task.kt
@@ -1,4 +1,8 @@
 package com.zuehlke.amongeus.tasks.model
 
-data class Task(val taskId: String, val lat: Float, val long: Float, val imgBase64:String, val creatorId: String, val gameId: String)
+data class Task(val taskId: String, val lat: Float, val long: Float, val imgBase64: String, val creatorId: String,
+                val gameId: String) {
+    var completed: Boolean = false
+    lateinit var assignedPlayerId: String
+}
 
