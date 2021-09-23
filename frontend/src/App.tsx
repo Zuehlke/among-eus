0fffe98c73f70a89d84ef7ddcc6fe8f84e24c08d
Nic <nicolas.odermatt@zuehlke.com>
2021-09-23 08:45:35 +0200

    Register player client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,6 +12,12 @@ interface AppState {
     client?: Client,
     games: Game[],
     playerName?: string,
+    playerClient?: Client,
+    playerId?: string
+}
+
+interface Player {
+    id: string,
 }
 
 class App extends React.Component<any, AppState> {
@@ -22,10 +28,13 @@ class App extends React.Component<any, AppState> {
             currentView: 'ChooseName',
             client: undefined,
             games: [],
+            playerClient: undefined,
+            playerId: '',
         };
         this.onNameChosen = this.onNameChosen.bind(this);
         this.onHostGame = this.onHostGame.bind(this);
         this.onGamesReceived = this.onGamesReceived.bind(this);
+        this.onPlayerReceived = this.onPlayerReceived.bind(this);
     }
 
     render() {
@@ -33,7 +42,7 @@ class App extends React.Component<any, AppState> {
         return (
             <div>
                 {currentView === 'ChooseName' && <ChooseName onNameChosen={this.onNameChosen}/>}
-                {currentView === 'ChooseGame' && <ChooseGame games={this.state.games} onHostGame={this.onHostGame}/>}
+                {currentView === 'ChooseGame' && <ChooseGame playerId={this.state.playerId} games={this.state.games} onHostGame={this.onHostGame}/>}
                 {currentView === 'Lobby' && <Lobby/>}
                 {currentView === 'CreateTasks' && <CreateTasks/>}
                 {currentView === 'SocketConnection' && <SocketConnection/>}
@@ -94,11 +103,46 @@ class App extends React.Component<any, AppState> {
         client.activate();
     }
 
+    initialiseWebSocketPlayers() {
+        const WS_PLAYERS_URL: string = (process.env.REACT_APP_WS_PLAYERS_URL as string);
+
+        const playerClient = new Client({
+            brokerURL: WS_PLAYERS_URL,
+            debug: function (str) {
+                console.debug(str);
+            },
+            reconnectDelay: 5000,
+            heartbeatIncoming: 4000,
+            heartbeatOutgoing: 4000,
+        });
+
+        playerClient.onConnect = () => {
+            this.setState({playerClient: playerClient});
+
+            // TODO: Change URL
+            playerClient.subscribe('/players', this.onGamesReceived)
+        }
+
+        playerClient.onStompError = (frame) => {
+            console.error('Broker reported error: ' + frame.headers['message']);
+            console.error('Additional details: ' + frame.body);
+        };
+
+        playerClient.activate();
+    }
+
 
     private onGamesReceived(message: Message) {
         const games: Game[] = JSON.parse(message.body);
         this.setState({
             games,
+        });
+    }
+
+    private onPlayerReceived(message: Message) {
+        const player: Player = JSON.parse(message.body);
+        this.setState({
+            playerId: player.id,
         });
     }
 }
