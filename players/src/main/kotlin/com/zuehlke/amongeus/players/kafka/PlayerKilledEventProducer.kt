6ccec800374733e66201c2c9cabe66950433282e
Rolf Bruderer <bruderer@computerscience.ch>
2021-09-23 13:21:13 +0200

    Missing service annotation

--- players/src/main/kotlin/com/zuehlke/amongeus/players/kafka/PlayerKilledEventProducer.kt
+++ players/src/main/kotlin/com/zuehlke/amongeus/players/kafka/PlayerKilledEventProducer.kt
@@ -3,7 +3,9 @@ package com.zuehlke.amongeus.players.kafka
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.zuehlke.amongeus.players.events.PlayerKilledEvent
 import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Service
 
+@Service
 class PlayerKilledEventProducer(val kafkaTemplate: KafkaTemplate<String, String>, val objectMapper : ObjectMapper) {
 
     fun playerKilled(gameId: String, playerId: String) {
