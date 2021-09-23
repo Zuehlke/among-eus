7cdcabcbf85b98f45349a39fe22786d95c351743
Schlumpf, Stefanie <stefanie.schlumpf2@zuehlke.com>
2021-09-23 08:37:34 +0200

    make game's list scrollable

--- frontend/src/components/ChooseGame/ChooseGame.tsx
+++ frontend/src/components/ChooseGame/ChooseGame.tsx
@@ -18,43 +18,35 @@ const ChooseGame = (props: any) => {
     }
 
     return (
-        <div className="ChooseGame">
-            <div className="flex-container full-height flex-flow-column">
-                <div className="header flex-container flex-flow-row">
-                    <img alt="Logo" src={logo}/>
-                </div>
-                <div className="component-element-container flex-grow">
-                    <div className="flex-container align-items-center">
-                        <button id="btnHostGame" onClick={props.onHostGame}>Host New Game</button>
-                    </div>
-                </div>
-                {props.games.length > 0 &&
-                <div className="component-element-container flex-grow overflow-hidden">
-                    <div className="height-100-percent flex-container flex-flow-row">
-                        {/*TODO make table scrollable */}
-                        <div className="flex-container align-items-center scroll">
-                            <table className="margin-left">
-                                <tbody>
-                                {props.games.map((game: Game) => {
-                                    return <tr className={selectedGameId === game.gameId ? selectedRowStyle : ''}
-                                               key={game.gameId}
-                                               onClick={() => selectGameId(game.gameId)}>
-                                        <td>{game.name}</td>
-                                    </tr>
-                                })}
-                                </tbody>
-                            </table>
-                        </div>
-                        <div className="flex-container align-items-center flex-grow">
-                            <button hidden={!selectedGameId} id="btnJoinGame"
-                                    onClick={() => console.log(selectedGameId)}>
-                                Join Game
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                }
+        <div className="ChooseGame flex-container full-height flex-flow-column overflow-hidden">
+            <div className="header">
+                <img alt="Logo" src={logo}/>
             </div>
+            <div className="button-container center">
+                <button id="btnHostGame" onClick={props.onHostGame}>Host New Game</button>
+            </div>
+            {props.games.length > 0 &&
+            <div className="flex-container flex-flow-column overflow-hidden">
+                <div className="component-element-container scroll">
+                    <table className="">
+                        <tbody>
+                        {props.games.map((game: Game) => {
+                            return <tr className={selectedGameId === game.gameId ? selectedRowStyle : ''}
+                                       key={game.gameId}
+                                       onClick={() => selectGameId(game.gameId)}>
+                                <td>{game.name}</td>
+                            </tr>
+                        })}
+                        </tbody>
+                    </table>
+                </div>
+                <div className="button-container center ">
+                   <button hidden={!selectedGameId} id="btnJoinGame" className="" onClick={() => console.log(selectedGameId)}>
+                        Join Game
+                    </button>
+                </div>
+            </div>
+            }
         </div>
     );
 }
--- frontend/src/components/ChooseName/ChooseName.tsx
+++ frontend/src/components/ChooseName/ChooseName.tsx
@@ -7,13 +7,13 @@ const ChooseName = (props: any) => {
     const [playerName, setPlayerName] = useState('');
 
     return (
-        <div>
+        <div className="ChooseName">
             <div className="logo">
                 <img alt="Logo" src={logo}/>
                 <p>Among Eus</p>
             </div>
             <div className="ChooseName center big-space-top">
-                <input type="text" placeholder="Enter your name" maxLength={20} onChange={event => setPlayerName(event.target.value)}/>
+                <input type="text" placeholder="Enter your name" maxLength={20} onChange={event => setPlayerName(event.target.value)} />
                 <button id="btnSubmit" disabled={!playerName} onClick={() => props.onNameChosen(playerName)} className="space-top" >Send</button>
             </div>
         </div>
