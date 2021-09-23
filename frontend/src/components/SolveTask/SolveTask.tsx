013d8b12676372b1b69bb1a084a6c3c49a7da582
Daniel Suter <suter_daniel@hotmail.com>
2021-09-23 13:33:31 +0200

    Clean up.

--- frontend/src/components/SolveTask/SolveTask.tsx
+++ frontend/src/components/SolveTask/SolveTask.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './SolveTask.css';
+import CreateTask from "../CreateTasks/CreateTask";
 
 const SolveTask = () => (
-  <div className="SolveTask">
-    SolveTask Component
-  </div>
+    <div className="SolveTask">
+        <CreateTask taskIndex={1} onChange={onChange}/>
+        <div>
+            <button onClick={solveTask}>Solve task</button>
+        </div>
+    </div>
 );
+
+function onChange() {
+
+}
+
+function solveTask() {
+
+}
 
 export default SolveTask;
