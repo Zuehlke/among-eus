1cd456d12497671f32780c32a2811e424a8e7c3e
Cagatay Suhan Özyurt <cagatayoezyurt@gmail.com>
2022-08-31 16:20:17 +0200

    Added test endpoint

--- among-eus-core/src/main/java/com/zuehlke/amongeus/core/controller/TestController.java
+++ among-eus-core/src/main/java/com/zuehlke/amongeus/core/controller/TestController.java
@@ -1,10 +1,15 @@
 package com.zuehlke.amongeus.core.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/test")
+@RequestMapping(value ="/test")
 public class TestController {
-    
+
+    @GetMapping
+    protected String getTest() {
+        return "Hallo Rolf";
+    }
 }
