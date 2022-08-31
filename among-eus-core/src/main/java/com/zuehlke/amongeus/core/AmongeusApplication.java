8c36bcb5148aa13925611713f48bb622c3b74171
Cagatay Suhan Özyurt <cagatayoezyurt@gmail.com>
2022-08-31 15:12:18 +0200

    Changed package name from base to core

--- among-eus-core/src/main/java/com/zuehlke/amongeus/core/AmongeusApplication.java
+++ among-eus-core/src/main/java/com/zuehlke/amongeus/core/AmongeusApplication.java
@@ -1,4 +1,4 @@
-package com.zuehlke.amongeus.base;
+package com.zuehlke.amongeus.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- among-eus-core/src/test/java/com/zuehlke/amongeus/core/AmongeusApplicationTests.java
+++ among-eus-core/src/test/java/com/zuehlke/amongeus/core/AmongeusApplicationTests.java
@@ -1,4 +1,4 @@
-package com.zuehlke.amongeus.base;
+package com.zuehlke.amongeus.core;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
