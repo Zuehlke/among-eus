e07b91b96215b0906b2b7a2e87e640fb607dd838
Cagatay Suhan Özyurt <cagatayoezyurt@gmail.com>
2022-08-31 16:02:46 +0200

    Changed project to gradle

--- among-eus-core/src/main/java/com/zuehlke/amongeus/core/CoreApplication.java
+++ among-eus-core/src/main/java/com/zuehlke/amongeus/core/CoreApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AmongeusApplication {
+public class CoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AmongeusApplication.class, args);
+		SpringApplication.run(CoreApplication.class, args);
 	}
 
 }
