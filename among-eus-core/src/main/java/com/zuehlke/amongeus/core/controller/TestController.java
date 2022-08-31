f53dd6e8009d2f86945b19d6627fb57a34594f40
Cagatay Suhan Özyurt <cagatayoezyurt@gmail.com>
2022-08-31 16:11:12 +0200

    Added gradle dependencies

---
+++ among-eus-core/src/main/java/com/zuehlke/amongeus/core/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.zuehlke.amongeus.core.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+    
+}
