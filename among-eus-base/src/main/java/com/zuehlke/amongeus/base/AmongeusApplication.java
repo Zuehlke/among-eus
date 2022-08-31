26967617e48e5dc6e6ccca8d0d9e82d20d060e2a
Cagatay Suhan Özyurt <cagatayoezyurt@gmail.com>
2022-08-31 14:54:54 +0200

    Added among-eus-base spring backend

---
+++ among-eus-base/src/main/java/com/zuehlke/amongeus/base/AmongeusApplication.java
@@ -0,0 +1,13 @@
+package com.zuehlke.amongeus.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmongeusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmongeusApplication.class, args);
+	}
+
+}
---
+++ among-eus-base/src/test/java/com/zuehlke/amongeus/base/AmongeusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuehlke.amongeus.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmongeusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
