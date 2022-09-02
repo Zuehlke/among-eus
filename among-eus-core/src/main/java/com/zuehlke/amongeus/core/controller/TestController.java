c27f9a3a18c77be248693452b75ad18464192e3d
Cagatay Suhan Özyurt <cagatayoezyurt@gmail.com>
2022-09-02 14:24:50 +0200

    Fixed project structure

--- among-eus-core/src/main/java/com/zuehlke/amongeus/core/controller/TestController.java
+++ among-eus-core/src/main/java/com/zuehlke/amongeus/core/controller/TestController.java
@@ -1,15 +1,17 @@
 package com.zuehlke.amongeus.core.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value ="/test")
+@RequestMapping(value = "/test")
 public class TestController {
 
     @GetMapping
-    protected String getTest() {
-        return "Hallo Rolf";
+    public ResponseEntity<String> getTest() {
+        return new ResponseEntity<>("Hello world", HttpStatus.OK);
     }
 }
