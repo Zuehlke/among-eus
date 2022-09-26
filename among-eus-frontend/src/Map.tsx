c55a643e043639c48940e527d7f6c6da78a65477
Fabio Stuber <fabio.stuber@gmail.com>
2022-09-26 15:00:15 +0200

    Add Map Options (No zooming, dragging or full-Screening)

--- among-eus-frontend/src/Map.tsx
+++ among-eus-frontend/src/Map.tsx
@@ -6,6 +6,31 @@ const render = (status: Status) => {
     return <h1>{status}</h1>;
 };
 
+const Marker: React.FC<google.maps.MarkerOptions> = (options) => {
+    const [marker, setMarker] = React.useState<google.maps.Marker>();
+
+    React.useEffect(() => {
+        if (!marker) {
+            setMarker(new google.maps.Marker());
+        }
+
+        // remove marker from map on unmount
+        return () => {
+            if (marker) {
+                marker.setMap(null);
+            }
+        };
+    }, [marker]);
+
+    React.useEffect(() => {
+        if (marker) {
+            marker.setOptions(options);
+        }
+    }, [marker, options]);
+
+    return null;
+};
+
 
 function MyMapComponent({center, zoom}: {
     center: google.maps.LatLngLiteral;
@@ -16,7 +41,7 @@ function MyMapComponent({center, zoom}: {
 
     useEffect(() => {
         if (ref.current && !map) {
-            setMap(new window.google.maps.Map(ref.current, {center, zoom}));
+            setMap(new window.google.maps.Map(ref.current, {center, zoom, scrollwheel: false, draggable: false, fullscreenControl: false}));
         }
     }, [ref, map]);
 
@@ -26,10 +51,11 @@ function MyMapComponent({center, zoom}: {
 export default function Map() {
     const center = { lat: 47.04423359340005, lng: 8.465418735471747 };
     const zoom = 20;
-
+    const position = { lat: 47.04423359340005, lng: 8.465418735471747 };
     return (
         <Wrapper apiKey="" render={render}>
             <MyMapComponent center={center} zoom={zoom} />
+            <Marker position={position} />
         </Wrapper>
     );
 }
