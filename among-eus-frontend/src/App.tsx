25efb1a0a4338e16929884c95d7a63feeebd6755
Suter, Daniel <daniel.suter@zuehlke.com>
2022-09-26 13:40:08 +0200

    Fix warning to enable deployment.

--- among-eus-frontend/src/App.tsx
+++ among-eus-frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import MapOverview from "./components/MapOverview/MapOverview";
 import {parseGameDetails} from "./utils/game-details";
