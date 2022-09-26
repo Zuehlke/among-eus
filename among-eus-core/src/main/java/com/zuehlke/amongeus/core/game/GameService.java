044f8e8ef4df1ca44e3a359b5491986bd23a3fa7
Rolf Bruderer <rolf.bruderer@ost.ch>
2022-09-26 14:53:01 +0200

    Clean up code packages and restructure code

--- among-eus-core/src/main/java/com/zuehlke/amongeus/core/game/GameService.java
+++ among-eus-core/src/main/java/com/zuehlke/amongeus/core/game/GameService.java
@@ -9,7 +9,6 @@ import java.util.concurrent.ConcurrentHashMap;
 @Service
 public class GameService {
 
-
     private final Map<String, Game> games = new ConcurrentHashMap<>();
 
     public Game getGame(String id) {
