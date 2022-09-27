05585a1c4a1025610481bc62ec8a721b78421a25
Andrea Mathis <andrea.mathis@zuehlke.com>
2022-09-27 16:47:12 +0200

    Calculate GameOver. TODO: send GameOver event.

--- among-eus-core/src/main/java/com/zuehlke/amongeus/core/model/Game.java
+++ among-eus-core/src/main/java/com/zuehlke/amongeus/core/model/Game.java
@@ -1,22 +1,21 @@
 package com.zuehlke.amongeus.core.model;
 
 
-import com.zuehlke.amongeus.core.utility.DistanceCalculator;
-
 import com.zuehlke.amongeus.core.task.TaskCreatedMessage;
+import com.zuehlke.amongeus.core.utility.DistanceCalculator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.Collection;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Game {
 
+    private static final Logger logger = LoggerFactory.getLogger(Game.class);
     public static final double MIN_DISTANCE_TO_KILL = 10;
 
     private String id;
     private GameState state = GameState.WAITING_FOR_PLAYERS;
-
     private Map<String, Player> players = new ConcurrentHashMap<>();
 
     private Map<String, Task> tasks = new ConcurrentHashMap<>();
@@ -36,37 +35,52 @@ public class Game {
     public void updatePlayer(Player player) {
         Optional<Player> playerOptional = Optional.ofNullable(players.get(player.getUsername()));
         playerOptional.ifPresent(value -> player.setAlive(value.isAlive()));
+        playerOptional.ifPresent(value -> player.setRole(value.getRole()));
         if (playerOptional.isEmpty() && !state.equals(GameState.WAITING_FOR_PLAYERS)) {
             throw new IllegalStateException("New player is not allowed to join, game is in state: "+ state);
         }
         players.put(player.getUsername(), player);
+        logger.info("Updated {}", player);
     }
 
     public Player killPlayer(String killerId, String killedId) {
-
         var killerPlayer = players.get(killerId);
         var killedPlayer = players.get(killedId);
 
-        if(!killedPlayer.isAlive() || !killerPlayer.isAlive()){
-            throw new IllegalStateException("Both players must be alive to kill!");
-        }
-        if(!isNearEnoughToKill(killerPlayer, killedPlayer)){
-            throw new IllegalStateException("Players are not near enough to kill!");
-        }
-        //TODO: is imposter
+        validateKilling(killerPlayer, killedPlayer);
         killedPlayer.setAlive(false);
         return killedPlayer;
     }
 
-    private boolean isNearEnoughToKill(Player killer, Player killed){
+    private boolean isNearEnoughToKill(Player killer, Player killed) {
         return DistanceCalculator.getDistanceInMeter(killer, killed) <= MIN_DISTANCE_TO_KILL;
     }
 
     public void startGame() {
-        if(state != GameState.WAITING_FOR_PLAYERS){
-            throw new IllegalStateException("Game is in "+ state +" state, and can not be started!");
+        logger.info("starting game ...");
+        if (state != GameState.WAITING_FOR_PLAYERS) {
+            throw new IllegalStateException("Game is in " + state + " state, and can not be started!");
         }
+        assignPlayerRoles();
         state = GameState.GAME_RUNNING;
+        logger.info("starting game ... done.");
+    }
+
+    private void assignPlayerRoles() {
+        getPlayers().forEach(p -> p.setRole(PlayerRole.AGENT));
+        var imposter = getRandomPlayer();
+        imposter.setRole(PlayerRole.TERRORIST);
+        logger.info("assigned player roles. Imposter is {}", imposter);
+    }
+
+    private Player getRandomPlayer() {
+        var playerList = new ArrayList<>(getPlayers());
+        var randomIndex = new Random().nextInt(playerList.size());
+        return playerList.get(randomIndex);
+    }
+
+    public void setState(GameState state) {
+        this.state = state;
     }
 
     public GameState getState() {
@@ -91,5 +105,40 @@ public class Game {
 
     public Collection<Task> getTasks() {
         return tasks.values();
+    }
+
+    private void validateKilling(Player killerPlayer, Player killedPlayer) {
+        if (!killedPlayer.isAlive() || !killerPlayer.isAlive()) {
+            throw new IllegalStateException("Both players must be alive to kill!");
+        }
+        if (!isNearEnoughToKill(killerPlayer, killedPlayer)) {
+            throw new IllegalStateException("Players are not near enough to kill!");
+        }
+        if (killerPlayer.getRole() != PlayerRole.TERRORIST) {
+            throw new IllegalStateException("User with role " + killerPlayer.getRole() + " can not kill");
+        }
+        if (state != GameState.GAME_RUNNING) {
+            throw new IllegalStateException("Can not kill in game state " + state);
+        }
+    }
+
+    public boolean isOver() {
+        var noRemainingTerrorist = getPlayers().stream()
+                .filter(p -> p.getRole() == PlayerRole.TERRORIST)
+                .noneMatch(Player::isAlive);
+        var noRemainingAgent = getPlayers().stream()
+                .filter(p -> p.getRole() == PlayerRole.AGENT)
+                .noneMatch(Player::isAlive);
+        return noRemainingTerrorist || noRemainingAgent;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" +
+                "id='" + id + '\'' +
+                ", state=" + state +
+                ", players=" + players +
+                ", tasks=" + tasks +
+                '}';
     }
 }
