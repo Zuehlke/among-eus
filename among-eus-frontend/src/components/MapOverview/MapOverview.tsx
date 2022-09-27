adc50f18853d54ce409b41bd4bf9b4474800c47d
Suter, Daniel <daniel.suter@zuehlke.com>
2022-09-27 13:04:48 +0200

    Reimplement map.

--- among-eus-frontend/src/components/MapOverview/MapOverview.tsx
+++ among-eus-frontend/src/components/MapOverview/MapOverview.tsx
@@ -2,15 +2,32 @@ import React, {FC} from 'react';
 import './MapOverview.css';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faCheck, faUser} from '@fortawesome/free-solid-svg-icons'
-import Map from "../../Map";
+import {Status, Wrapper} from '@googlemaps/react-wrapper';
+import PlayerMap from "./PlayerMap/PlayerMap";
+import Marker, {MarkerTypes} from "./PlayerMap/Marker";
 
 interface MapOverviewProps {
     userId: string | null;
     gameId: string | null;
 }
 
+const renderMapStatus = (status: Status) => {
+    return <h1>{status}</h1>;
+};
+
 const MapOverview: FC<MapOverviewProps> = (props) => {
     startGpsTracking();
+
+    // TODO use GPS positioning
+    const center = {
+        lat: 47.0443195,
+        lng: 8.465423,
+    }
+
+    const otherPlayer = {
+        lat: 47.0444195,
+        lng: 8.466423,
+    }
 
     return (
         <div>
@@ -20,7 +37,12 @@ const MapOverview: FC<MapOverviewProps> = (props) => {
                 icon={faCheck}/> 5
                 Tasks
             </div>
-            <Map />
+            <Wrapper apiKey="AIzaSyC3PzqgCWeT_lrobprlTEz1SmVQ443n2Mg" render={renderMapStatus}>
+                <PlayerMap center={center} zoom={18}>
+                    <Marker key={1} position={center} labelName={'Me'} labelType={MarkerTypes.PLAYER}></Marker>
+                    <Marker key={2} position={otherPlayer} labelName={'Fabio'} labelType={MarkerTypes.OPPONENT}></Marker>
+                </PlayerMap>
+            </Wrapper>
             <div className="action-bar">
                 <div className="action-bar-child">Daniel (7 Meter)</div>
                 <div className="action-bar-child">
