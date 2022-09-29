563d8990d74fe808a70bbd906703bdfa9ab40fbc
Rolf Bruderer <rolf.bruderer@ost.ch>
2022-09-29 09:58:58 +0200

    Quick fix to send out player list in case of game start (additionally needed for easier debugging)

--- among-eus-core/src/main/java/com/zuehlke/amongeus/core/player/PlayerController.java
+++ among-eus-core/src/main/java/com/zuehlke/amongeus/core/player/PlayerController.java
@@ -57,6 +57,7 @@ public class PlayerController {
         logger.info("Starting game: {}", gameId);
         Game game = gameService.getGame(gameId);
         game.startGame(gameStartConfigurationMessage);
+        sendPlayerList(game);
         return game.getState();
     }
 
